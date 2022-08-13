700e9d0989f7f6dc14be393e5673dd2ba63fbbaf
Stanislav Sporevoi <stanislav.sporevoi@gmail.com>
2022-08-13 22:02:10 +0300

    prmary tasks finished

--- src/index.js
+++ src/index.js
@@ -5,27 +5,76 @@ import Notiflix from "notiflix";
 const refs = {
     form: document.querySelector('.search-form'),
     input: document.querySelector('input'),
-    button: document.querySelector('button'),
-    gallery: document.querySelector('.gallery')
+    searchBtn: document.querySelector('button'),
+    gallery: document.querySelector('.gallery'),
+    loadMoreBtn: document.querySelector('.load-more'),
 };
-refs.form.addEventListener('submit', fetchCards)
-async function fetchCards(event) {
+let limit = 100;
+let page = 0;
+let totalPages;
+
+refs.loadMoreBtn.classList.add('is-hidden');
+refs.form.addEventListener('submit', fetchOnSubmit)
+
+function fetchOnSubmit(event) {
     event.preventDefault();
+    page = 1;
+    clearGallery();
+    fetchCards();
+}
+
+async function fetchCards() {    
+    try {
+        const response = await axios.get(createURL());
+        const data = response.data.hits;
+        let amount = response.data.totalHits
+        totalPages = Math.ceil(amount/limit);
+        console.log(totalPages)
+
+        if (data.length === 0) {
+            refs.loadMoreBtn.classList.add('is-hidden');
+            Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+            return;
+        }
+
+        if (page === 1) {
+            Notiflix.Notify.success(`Hooray! We found ${amount} images.`)
+        }
+
+        if (page > totalPages) {
+            refs.loadMoreBtn.classList.add('is-hidden');
+            Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+            return;
+        }
+        page += 1;
+        refs.loadMoreBtn.classList.remove('is-hidden');
+        refs.loadMoreBtn.addEventListener('click', fetchCards);
+        
+        renderCards(data);
+        return console.log(data);
+    }
+    catch (error) {
+    console.log(error);
+    }
+    
+}
+function createURL() {
     const name = refs.input.value.trim();
     const API_KEY = '29184365-ad7d7355f63935605b47c8dfc';
     const BASE_URL = 'https://pixabay.com/api';
-
-    const response = await axios.get(`${BASE_URL}/?key=${API_KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true`)
-    const data = response.data.hits;
-    // return data;
-    renderCards(data)
+    const url = `${BASE_URL}/?key=${API_KEY}&q=${name}&page=${page}&per_page=${limit}&image_type=photo&orientation=horizontal&safesearch=true`;
+    return url;
 }
-
 
 function renderCards(items) {
     const markup = items
-      .map(({ webformatURL, tags, likes, views, comments, downloads }) => {
-          return `
+      .map(createCard)
+      .join("");
+    refs.gallery.insertAdjacentHTML('beforeend', markup);
+}
+
+function createCard({ webformatURL, tags, likes, views, comments, downloads }) {
+    return `
             <div class="photo-card">
                 <img class="photo-card__image" src="${webformatURL}" alt="${tags}" loading="lazy" width= '180px' />
                 <div class="info">
@@ -47,10 +96,9 @@ function renderCards(items) {
                     </p>
                 </div>
             </div>`
-      })
-      .join("");
-    refs.gallery.innerHTML = markup;
 }
 
-
+function clearGallery() {
+    refs.gallery.innerHTML = "";
+}
 
