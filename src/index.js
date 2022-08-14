92117fa3defc2318406495abbf3d3dd81e31157e
Stanislav Sporevoi <stanislav.sporevoi@gmail.com>
2022-08-14 14:03:53 +0300

    limit 40

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const refs = {
     gallery: document.querySelector('.gallery'),
     loadMoreBtn: document.querySelector('.load-more'),
 };
-let limit = 100;
+let limit = 40;
 let page = 0;
 let totalPages;
 
